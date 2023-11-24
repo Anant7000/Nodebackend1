26879016bdc8bc3cc5450e12650367c269cd0200
Anant7000 <127724636+Anant7000@users.noreply.github.com>
2023-11-24 15:12:41 +0530

    Update index.js
    Ram

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Jai Shree Ram')
 })
 
 app.listen(PORT, function() {
